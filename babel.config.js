ebedc50517c4ad85e9c3ea832206c77101e8573e
thaito <thai.to@kardiachain.io>
2023-10-11 10:58:29 +0700

    Fix Flatlist render error on Android: https://github.com/facebook/react-native/issues/36828

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ["@babel/plugin-transform-private-methods", { "loose": true }],
-    "react-native-reanimated/plugin"
-  ]
+    ['@babel/plugin-transform-flow-strip-types', {loose: true}],
+    ['@babel/plugin-transform-private-methods', {loose: true}],
+    'react-native-reanimated/plugin',
+  ],
 };
