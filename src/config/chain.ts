a8e57238bbfc6054f0654780cd77ef4c0da011e2
phongnhat19 <phongnhat19@gmail.com>
2023-11-06 15:47:36 +0700

    change config URL to main repo

--- src/config/chain.ts
+++ src/config/chain.ts
@@ -22,15 +22,15 @@ export const SUPPORTED_CHAINS: NetworkConfig[] = [
     rpc: "https://rpc-mainnet.uniultra.xyz",
     explorerURL: "https://u2uscan.xyz/",
     currency: "U2U",
-    suppoted_tokens_endpoint: "https://raw.githubusercontent.com/phongnhat19/explorer-assets/master/mobile_config/supported_tokens_mainnet.json",
-    remote_config_endpoint: "https://raw.githubusercontent.com/phongnhat19/explorer-assets/master",
+    suppoted_tokens_endpoint: "https://raw.githubusercontent.com/unicornultrafoundation/explorer-assets/master/mobile_config/supported_tokens_mainnet.json",
+    remote_config_endpoint: "https://raw.githubusercontent.com/unicornultrafoundation/explorer-assets/master",
     stakingAddress: "0xfc00face00000000000000000000000000000000",
     sfcSubgraph: "https://graph.uniultra.xyz/subgraphs/name/u2u/sfc-subgraph-v1",
     u2uNetworkSubgraph: "https://graph.uniultra.xyz/subgraphs/name/u2u/sfc-network",
     stakingGraphql: "https://staking-graphql.uniultra.xyz/graphql",
     withdrawPeriodTime: 604800,
-    dappURL: "https://raw.githubusercontent.com/phongnhat19/explorer-assets/master/mobile_config/dapp_mainnet.json",
-    ecosystemSettingURL: "https://raw.githubusercontent.com/phongnhat19/explorer-assets/master/mobile_config/ecosystem_setting_mainnet.json"
+    dappURL: "https://raw.githubusercontent.com/unicornultrafoundation/explorer-assets/master/mobile_config/dapp_mainnet.json",
+    ecosystemSettingURL: "https://raw.githubusercontent.com/unicornultrafoundation/explorer-assets/master/mobile_config/ecosystem_setting_mainnet.json"
   },
   {
     chainID: "2484",
@@ -38,15 +38,15 @@ export const SUPPORTED_CHAINS: NetworkConfig[] = [
     rpc: "https://rpc-nebulas-testnet.uniultra.xyz",
     explorerURL: "https://testnet.u2uscan.xyz",
     currency: "U2U",
-    suppoted_tokens_endpoint: "https://raw.githubusercontent.com/phongnhat19/explorer-assets/master/mobile_config/supported_tokens.json",
-    remote_config_endpoint: "https://raw.githubusercontent.com/phongnhat19/explorer-assets/master",
+    suppoted_tokens_endpoint: "https://raw.githubusercontent.com/unicornultrafoundation/explorer-assets/master/mobile_config/supported_tokens.json",
+    remote_config_endpoint: "https://raw.githubusercontent.com/unicornultrafoundation/explorer-assets/master",
     stakingAddress: "0xfc00face00000000000000000000000000000000",
     // sfcSubgraph: "https://subgraph.uniultra.xyz/subgraphs/name/u2u/sfc-subgraph",
     sfcSubgraph: "https://5a1e-171-242-12-100.ngrok-free.app/subgraphs/name/u2u/sfc-subgraph",
     u2uNetworkSubgraph: "https://subgraph.uniultra.xyz/subgraphs/name/u2u/sfc-network",
     stakingGraphql: "https://testnet-staking-graphql.uniultra.xyz/graphql",
     withdrawPeriodTime: 604800,
-    dappURL: "https://raw.githubusercontent.com/phongnhat19/explorer-assets/master/mobile_config/dapp.json",
-    ecosystemSettingURL: "https://raw.githubusercontent.com/phongnhat19/explorer-assets/master/mobile_config/ecosystem_setting.json"
+    dappURL: "https://raw.githubusercontent.com/unicornultrafoundation/explorer-assets/master/mobile_config/dapp.json",
+    ecosystemSettingURL: "https://raw.githubusercontent.com/unicornultrafoundation/explorer-assets/master/mobile_config/ecosystem_setting.json"
   }
 ]
