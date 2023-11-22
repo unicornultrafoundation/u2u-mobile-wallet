3f7d6a115c92e371ef7e0205ae3cb1b0bfb746f2
Trang Le Thi Quynh <trangltq@timebird.org>
2023-11-22 12:18:34 +0700

    [U2USA-13][BUG] Clear textfied at SendTokenScreen

--- src/screen/StakingDashboardScreen/InvestmentTotalCard.tsx
+++ src/screen/StakingDashboardScreen/InvestmentTotalCard.tsx
@@ -70,7 +70,7 @@ const InvestmentTotalCard = () => {
             }
           ]}
         >
-          {t('balance')}
+          {t('investedBalance')}
         </Text>
         <Text
           style={[
