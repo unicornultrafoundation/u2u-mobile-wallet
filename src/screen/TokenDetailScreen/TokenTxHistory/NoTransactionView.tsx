5d7ee005c9931bff375488c729b312a97f133246
Trang Le Thi Quynh <trangltq@timebird.org>
2023-11-30 09:48:41 +0700

    [U2USA-21][BUG] Update UI for transaction history

--- src/screen/TokenDetailScreen/TokenTxHistory/NoTransactionView.tsx
+++ src/screen/TokenDetailScreen/TokenTxHistory/NoTransactionView.tsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
 import { useTranslation } from "react-i18next";
+import { darkTheme, lightTheme } from '../../../theme/color';
+import { usePreferenceStore } from '../../../state/preferences';
+import theme from '../../../theme';
 
 const NoTransactionView = () => {
 
   const {t} = useTranslation()
+  const {darkMode} = usePreferenceStore()
+  const preferenceTheme = darkMode ? darkTheme : lightTheme
 
   return (
     <View style={styles.container}>
       <Image source={require('../../../asset/images/ic_no_transaction.png')} style={styles.image} />
-      <Text style={styles.text}> {t('There is no transaction to show')}</Text>
+      <Text style={[
+        theme.typography.body.regular,
+        {
+          color: preferenceTheme.text.disabled,
+        }
+      ]}> {t('There is no transaction to show')}</Text>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 80,
-    justifyContent: 'flex-start',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginVertical: 60,
+    marginHorizontal: 16,
   },
   image: {
     width: 120,
     height: 120,
-    marginBottom: 20,
-  },
-  text: {
-    fontSize: 16,
-    color: 'gray',
+    objectFit: 'contain',
+    marginBottom: 16,
   },
 });
 
--- src/screen/TokenDetailScreen/TokenTxHistory/index.tsx
+++ src/screen/TokenDetailScreen/TokenTxHistory/index.tsx
@@ -3,18 +3,14 @@ import React from 'react'
 import { ActivityIndicator, ScrollView, TouchableOpacity, View } from 'react-native';
 import { useTokenTxHistory } from '../../../hook/useTokenTxHistory';
 import { useWallet } from '../../../hook/useWallet';
-import { styles } from './styles';
-import Text from '../../../component/Text';
-import Icon from '../../../component/Icon';
-import { shortenAddress } from '../../../util/string';
-import { parseFromRaw } from '../../../util/bignum';
-import { formatDate } from '../../../util/date';
 import theme from '../../../theme';
 import Button from '../../../component/Button';
 import { usePreferenceStore } from '../../../state/preferences';
 import { darkTheme, lightTheme } from '../../../theme/color';
 import NoTransactionView from "./NoTransactionView";
 import { useTranslation } from "react-i18next";
+import TxHistoryItem from '../../TxHistoryScreen/TxHistoryItem';
+import LoadingView from '../../../component/Common/loadingView';
 
 const TokenTxHistory = () => {
   const {params} = useRoute<any>();
@@ -30,75 +26,33 @@ const TokenTxHistory = () => {
   const preferenceTheme = darkMode ? darkTheme : lightTheme
 
   if (loading) {
-    return (
-      <View>
-        <ActivityIndicator />
-      </View>
-    )
+    return <LoadingView/>
   }
 
   if((txList ?? []).length == 0){
     return <NoTransactionView/>
   }
-
+  
   return (
     <ScrollView>
       {txList.map((txItem: Record<string, any>) => {
-        const isSend = wallet.address.toLowerCase() === txItem.from.toLowerCase()
-        return (
-          <TouchableOpacity
-            style={styles.txRowContainer}
-            key={`token-tx-${txItem.hash}`}
-            onPress={() => navigation.navigate("TransactionDetail", {transactionHash: txItem.hash})}
-          >
-            <Icon
-              name={isSend ? 'arrow-up-circle' : 'arrow-down-circle'}
-              color={isSend ? theme.accentColor.error.normal : theme.accentColor.tertiary.normal}
-              width={20}
-              height={20}
-              style={{marginRight: 24}}
-            />
-            <View style={{flex: 1}}>
-              <Text style={styles.txTypeText}>
-                {isSend ? t("send") : t("receive")}
-              </Text>
-              <Text style={styles.txTypeDescriptionText}>
-                {isSend ? `To: ${shortenAddress(txItem.to, 8, 5)}` : `From: ${shortenAddress(txItem.from, 8, 5)}`}
-              </Text>
-            </View>
-            <View style={{alignItems: 'flex-end'}}>
-              <Text style={[
-                styles.amountText,
-                {color: isSend ? theme.accentColor.error.normal : theme.accentColor.tertiary.normal}
-              ]}>
-                {isSend ? '-' : '+'}{parseFromRaw(txItem.value, txItem.tokenDecimal || 18, true)}
-              </Text>
-              <Text style={styles.dateText}>
-                {formatDate(Number(txItem.timeStamp) * 1000, 'yyyy-MM-dd')}
-              </Text>
-            </View>
-          </TouchableOpacity>
-        )
+        return <TxHistoryItem txItem={txItem}/>
       })}
       <View style={{paddingVertical: 20, alignItems: 'center', justifyContent: 'center'}}>
         <Button
           style={{
-            width: 120,
-            // height: 34,
-            borderRadius: 80,
+            borderRadius: 60,
             paddingHorizontal: 16,
-            paddingVertical: 12,
+            paddingVertical: 10,
             backgroundColor: preferenceTheme.background.surface
           }}
           color='tertiary'
-          textStyle={{
-            fontSize: 12,
-            lineHeight: 16,
-            fontWeight: '500',
-            color: theme.color.neutral[500]
-          }}
+          textStyle={[
+            theme.typography.caption1.medium,
+            {color: preferenceTheme.text.disabled}
+          ]}
         >
-          View more
+          {t('viewMore')}
         </Button>
       </View>
     </ScrollView>
