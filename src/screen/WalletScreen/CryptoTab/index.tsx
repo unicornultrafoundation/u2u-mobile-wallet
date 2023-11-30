9a5f8b60f2cadab04cb91b4d0a142a6fd42dc7ee
David Huan <huantd@timebird.org>
2023-11-30 09:35:17 +0700

    Update UI on Wallet screen

--- src/screen/WalletScreen/CryptoTab/index.tsx
+++ src/screen/WalletScreen/CryptoTab/index.tsx
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useWalletAssets } from '../../../hook/useWalletAssets';
-import { ScrollView } from 'react-native';
 import TokenRow from './TokenRow';
-import { useGlobalStore } from '../../../state/global';
 import { useLocalStore } from '../../../state/local';
+import { FlatList, NativeScrollEvent, NativeSyntheticEvent } from 'react-native';
+import { useGlobalStore } from '../../../state/global';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const CryptoTab = () => {
-  const {assets, assetsToShow} = useWalletAssets()
-  const {searchKeyword} = useGlobalStore()
+const CryptoTab = ({ collapsed, onResetParentView }: { collapsed: boolean, onResetParentView: () => void; }) => {
+  const { assets, assetsToShow } = useWalletAssets()
+  const [firstTouch, setFirstTouch] = useState(0);
+  const { searchKeyword } = useGlobalStore()
 
-  const {selectedToken, tokenListInitted, saveSelectedToken, setTokenListInitted} = useLocalStore()
+  const { selectedToken, tokenListInitted, saveSelectedToken, setTokenListInitted } = useLocalStore()
+
+  // Handle Swipe event
+  const onScrollEndDrag = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    console.log(`CryptoTab onScrollEndDrag ${e.nativeEvent.contentOffset.y}`)
+    if (e.nativeEvent.contentOffset.y <= 0 && firstTouch == 0 && collapsed) {
+      onResetParentView();
+    }
+  };
+
+  const onScrollBeginDrag = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    console.log(`CryptoTab onScrollBeginDrag ${e.nativeEvent.contentOffset.y}`)
+    setFirstTouch(e.nativeEvent.contentOffset.y);
+  };
+
 
   useEffect(() => {
     if (tokenListInitted) return
@@ -23,19 +39,43 @@ const CryptoTab = () => {
 
   }, [tokenListInitted, selectedToken, assets])
 
-  return (
-    <ScrollView>
+  if (collapsed) {
+    return <ScrollView
+      nestedScrollEnabled={true}
+      contentContainerStyle={{
+        flexGrow: 1,
+        minHeight: 500
+      }}
+      onScrollBeginDrag={e => onScrollBeginDrag(e)}
+      onScrollEndDrag={e => onScrollEndDrag(e)}
+    >
       {assetsToShow.filter((i) => {
         return (i.address as string).toLowerCase().includes(searchKeyword.toLowerCase()) ||
-          (i.symbol as string).toLowerCase().includes(searchKeyword.toLowerCase()) || 
+          (i.symbol as string).toLowerCase().includes(searchKeyword.toLowerCase()) ||
           (i.name as string).toLowerCase().includes(searchKeyword.toLowerCase())
-      }).map((tokenObj: any) => {
+      }).map((item: any) => {
         return (
-          <TokenRow tokenObj={tokenObj} key={`token-asset-${tokenObj.symbol}`} />
+          <TokenRow tokenObj={item} key={`token-asset-${item.symbol}-${item.name}`} />
         )
       })}
     </ScrollView>
-  )
+  } else {
+    return (
+      <FlatList
+        nestedScrollEnabled={true}
+        style={{ minHeight: 500, flexGrow: 0, }}
+        onScrollBeginDrag={e => onScrollBeginDrag(e)}
+        onScrollEndDrag={e => onScrollEndDrag(e)}
+        data={assetsToShow.filter((i) => {
+          return (i.address as string).toLowerCase().includes(searchKeyword.toLowerCase()) ||
+            (i.symbol as string).toLowerCase().includes(searchKeyword.toLowerCase()) ||
+            (i.name as string).toLowerCase().includes(searchKeyword.toLowerCase())
+        })}
+        renderItem={({ item }) => <TokenRow tokenObj={item} key={`token-asset-${item}`} />}
+        keyExtractor={item => `token-asset-${item.symbol}-${item.name}`}
+      />
+    )
+  }
 }
 
 export default CryptoTab;
--- src/screen/WalletScreen/NFTTab.tsx
+++ src/screen/WalletScreen/NFTTab.tsx
@@ -5,6 +5,8 @@ import {
   Image,
   ScrollView,
   Dimensions,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
 } from 'react-native';
 import Text from '../../component/Text';
 import { usePreferenceStore } from '../../state/preferences';
@@ -17,14 +19,16 @@ import NFTRow from './NFTRow';
 import theme from '../../theme';
 import { styles } from './styles';
 import { useTranslation } from 'react-i18next';
+import { FlatList } from 'react-native';
 
-const NFTTab = () => {
-  const {t} = useTranslation()
+const NFTTab = ({ collapsed, onResetParentView }: { collapsed: boolean, onResetParentView: () => void; }) => {
+  const { t } = useTranslation()
   const { darkMode } = usePreferenceStore();
   const preferenceTheme = darkMode ? darkTheme : lightTheme;
   const navigation = useNavigation<any>();
+  const [firstTouch, setFirstTouch] = useState(0);
 
-  const {supportedNFT: data} = useSupportedNFT()
+  const { supportedNFT: data } = useSupportedNFT()
 
   const [expandedItem, setExpandedItem] = useState("");
   const handleExpandItem = (id: string) => {
@@ -35,28 +39,69 @@ const NFTTab = () => {
     }
   };
 
-  return (
-    <View style={{ paddingHorizontal: 16 }}>
-      {/* <Dropdown
-        containerStyle={{ marginBottom: 16 }}
-        renderList={<Text style={{ color: 'white' }}>List</Text>}>
-        <Text style={{ color: preferenceTheme.text.title, fontSize: 14 }}>
-          All collectibles
-        </Text>
-      </Dropdown> */}
+  // Handle Swipe event
+  const onScrollEndDrag = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    console.log(`onScrollEndDrag ${e.nativeEvent.contentOffset.y}`)
+    if (e.nativeEvent.contentOffset.y <= 0 && firstTouch == 0 && collapsed) {
+      onResetParentView();
+    }
+  };
 
-      {(data ?? []).length == 0 && (
-       <View style={styles.containerNoNFT}>
-          <Image source={require('../../asset/images/ic_no_nft.png')} style={styles.imageNoNFT} resizeMode="contain"/>
-          <Text style={styles.textNoNFT}> {t('There is no NFTs yet')}</Text>
-       </View>
+  const onScrollBeginDrag = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    console.log(`onScrollBeginDrag ${e.nativeEvent.contentOffset.y}`)
+    setFirstTouch(e.nativeEvent.contentOffset.y);
+  };
+
+  if (collapsed) {
+    return <ScrollView
+      nestedScrollEnabled={true}
+      contentContainerStyle={{
+        flexGrow: 1,
+        minHeight: 500,
+        paddingHorizontal: 16
+      }}
+      onScrollBeginDrag={e => onScrollBeginDrag(e)}
+      onScrollEndDrag={e => onScrollEndDrag(e)}
+    >
+      {data.map(
+        (item) => (
+          <NFTRow key={`nft-${item.id}`} nftCollection={item} open={expandedItem === item.id} handleExpandItem={handleExpandItem} />
+        )
       )}
+    </ScrollView>
 
-      {data.map((item) => (
-        <NFTRow key={`nft-${item.id}`} nftCollection={item} open={expandedItem === item.id} handleExpandItem={handleExpandItem} />
-      ))}
-    </View>
-  );
+  } else {
+    return (
+      <View style={{ paddingHorizontal: 16 }}>
+        {/* <Dropdown
+          containerStyle={{ marginBottom: 16 }}
+          renderList={<Text style={{ color: 'white' }}>List</Text>}>
+          <Text style={{ color: preferenceTheme.text.title, fontSize: 14 }}>
+            All collectibles
+          </Text>
+        </Dropdown> */}
+
+        {(data ?? []).length == 0 && (
+          <View style={styles.containerNoNFT}>
+            <Image source={require('../../asset/images/ic_no_nft.png')} style={styles.imageNoNFT} resizeMode="contain" />
+            <Text style={styles.textNoNFT}> {t('There is no NFTs yet')}</Text>
+          </View>
+        )}
+
+
+        <FlatList
+          nestedScrollEnabled={true}
+          style={{ minHeight: 500 }}
+          onScrollBeginDrag={e => onScrollBeginDrag(e)}
+          onScrollEndDrag={e => onScrollEndDrag(e)}
+          data={data}
+          renderItem={({ item }) => <NFTRow key={`nft-${item.id}`} nftCollection={item} open={expandedItem === item.id} handleExpandItem={handleExpandItem} />}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    );
+  }
+
 };
 
 export default NFTTab;
