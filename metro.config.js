1aa74895abc26debaeaaa9f582a83d845fe7543b
phongnhat19 <phongnhat19@gmail.com>
2023-12-03 13:26:29 +0700

    update obfuscate setting

--- metro.config.js
+++ metro.config.js
@@ -7,6 +7,26 @@ const defaultConfig = getDefaultConfig(__dirname);
  *
  * @type {import('metro-config').MetroConfig}
  */
+
+const jsoMetroPlugin = require("obfuscator-io-metro-plugin")(
+  {
+    // for these option look javascript-obfuscator library options from  above url
+    compact: false,
+    sourceMap: false, // source Map generated after obfuscation is not useful right now so use default value i.e. false
+    controlFlowFlattening: true,
+    controlFlowFlatteningThreshold: 1,
+    numbersToExpressions: true,
+    simplify: true,
+    stringArrayShuffle: true,
+    splitStrings: true,
+    stringArrayThreshold: 1,
+  },
+  {
+    runInDev: false /* optional */,
+    logObfuscatedFiles: true /* optional generated files will be located at ./.jso */,
+  }
+);
+
 const config = {
   transformer: {
     getTransformOptions: async () => ({
@@ -18,7 +38,8 @@ const config = {
   },
   resolver: {
     assetExts: [...defaultConfig.resolver.assetExts, "jsstring"]
-  }
+  },
+  ...jsoMetroPlugin,
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
