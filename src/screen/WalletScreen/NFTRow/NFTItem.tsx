06162cc2847250c9bf6eb60634c62afaafbb1434
phongnhat19 <phongnhat19@gmail.com>
2023-12-28 16:20:54 +0700

    implement image cache

--- src/screen/WalletScreen/NFTRow/NFTItem.tsx
+++ src/screen/WalletScreen/NFTRow/NFTItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { OwnedNFT } from '../../../hook/useOwnedNFT';
 import { TouchableOpacity, Image } from 'react-native';
+import { CachedImage } from '@georstat/react-native-image-cache';
 import { useNFTMetadata } from '../../../hook/useNFTMetadata';
 import { parseIPFSFile } from '../../../util/string';
 
@@ -17,7 +18,7 @@ const NFTItem = ({item, onClick}: {
       style={{ width: 111, height: 111 }}
       onPress={onClick}
     >
-      <Image
+      {/* <Image
         source={{ uri: parseIPFSFile(data.image) }}
         style={{
           width: '100%',
@@ -25,6 +26,16 @@ const NFTItem = ({item, onClick}: {
           objectFit: 'cover',
           borderRadius: 8,
         }}
+      /> */}
+      <CachedImage
+        source={parseIPFSFile(data.image)}
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          borderRadius: 8,
+        }}
+        thumbnailSource="https://via.placeholder.com/111x111"
       />
     </TouchableOpacity>
   )
