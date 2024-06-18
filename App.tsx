cc3e39820b54fb5dd88f993d8e7f0e06e35eab00
phongnhat19 <phongnhat19@gmail.com>
2024-06-18 19:15:11 +0700

    implement Toast component

--- App.tsx
+++ App.tsx
@@ -8,8 +8,8 @@
 import 'react-native-gesture-handler';
 import '@ethersproject/shims';
 import 'event-target-polyfill'
-import React, { useEffect, useMemo } from 'react';
-import { Linking, StatusBar, TouchableOpacity, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Linking, StatusBar } from 'react-native';
 
 import { NavigationContainer, NavigationContainerRef } from '@react-navigation/native';
 import analytics from '@react-native-firebase/analytics';
@@ -26,10 +26,6 @@ import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { useNetInfo } from "@react-native-community/netinfo";
 // import appsFlyer from 'react-native-appsflyer';
 import { MenuProvider } from 'react-native-popup-menu';
-import Toast from 'react-native-toast-message';
-import theme from './src/theme';
-import Text from './src/component/Text';
-import Icon from './src/component/Icon';
 import { useNetwork } from './src/hook/useNetwork';
 import { useGlobalStore } from './src/state/global';
 import AuthScreen from './src/screen/AuthScreen';
@@ -44,6 +40,7 @@ import MainTabNav from './src/stack/MainTab';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useCrashlytics } from './src/hook/useCrashlytics';
 import { useNotifications } from './src/hook/useNotifications';
+import ToastComponent from './src/component/Toast';
 
 //@ts-ignore
 global.CustomEvent = global.Event
@@ -189,157 +186,6 @@ function App(): JSX.Element {
     submitDeviceNotiToken()
   }, [submitDeviceNotiToken])
 
-  const toastConfig = useMemo(() => {
-    return {
-      success: ({text1, text2, props}: any) => {
-        return (
-          <View
-            style={{
-              height: 60,
-              width: '85%',
-              padding: 12,
-              borderRadius: 16,
-              backgroundColor: preferenceTheme.background.surface,
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginTop: 16
-            }}
-          >
-            <View style={{padding: 10, paddingRight: 0}}>
-              <Icon name='success' height={32} width={32} />
-            </View>
-            <View style={{flex: 1, paddingHorizontal: 12}}>
-              <Text
-                style={[
-                  theme.typography.body.bold,
-                  {
-                    color: preferenceTheme.text.title
-                  }
-                ]}
-              >
-                {text1}
-              </Text>
-              {props.txHash && (
-                <TouchableOpacity
-                  style={{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}
-                  onPress={() => {
-                    Linking.openURL(`${blockExplorer}/tx/${props.txHash.toLowerCase()}`)
-                  }}
-                >
-                  <Text
-                    style={[
-                      theme.typography.body.medium,
-                      {
-                        color: preferenceTheme.text.disabled
-                      }
-                    ]}
-                  >
-                    {t('detail')}
-                  </Text>
-                  <Icon name="chevron-right" width={18} height={18} />
-                </TouchableOpacity>
-              )}
-            </View>
-            {props.renderTrailing && props.renderTrailing()}
-          </View>
-        )
-      },
-      error: ({text1, text2, props}: any) => {
-        return (
-          <View
-            style={{
-              height: 60,
-              width: '85%',
-              padding: 12,
-              borderRadius: 16,
-              backgroundColor: preferenceTheme.background.surface,
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginTop: 16
-            }}
-          >
-            <View style={{padding: 10}}>
-              <Icon name='error' height={32} width={32} />
-            </View>
-            <View style={{flex: 1, paddingHorizontal: 12}}>
-              <Text
-                style={[
-                  theme.typography.body.medium,
-                  {
-                    color: preferenceTheme.text.title
-                  }
-                ]}
-              >
-                {text1}
-              </Text>
-              <Text
-                style={[
-                  theme.typography.body.medium,
-                  {
-                    color: preferenceTheme.text.disabled
-                  }
-                ]}
-              >
-                {text2}
-              </Text>
-            </View>
-            {props.renderTrailing && props.renderTrailing()}
-          </View>
-        )
-      },
-      simpleNoti: ({text1, text2, props}: any) => {
-        return (
-          <View
-            style={{
-              height: 40,
-              padding: 8,
-              borderRadius: 12,
-              backgroundColor: preferenceTheme.background.surface,
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginTop: 16
-            }}
-          >
-            <View style={{flex: 1, paddingHorizontal: 12}}>
-              <Text
-                style={[
-                  theme.typography.body.bold,
-                  {
-                    color: preferenceTheme.text.title,
-                    textAlign: 'center'
-                  }
-                ]}
-              >
-                {text1}
-              </Text>
-              {props.txHash && (
-                <TouchableOpacity
-                  style={{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}
-                  onPress={() => {
-                    Linking.openURL(`${blockExplorer}/tx/${props.txHash.toLowerCase()}`)
-                  }}
-                >
-                  <Text
-                    style={[
-                      theme.typography.body.medium,
-                      {
-                        color: preferenceTheme.text.disabled
-                      }
-                    ]}
-                  >
-                    {t('detail')}
-                  </Text>
-                  <Icon name="chevron-right" width={18} height={18} />
-                </TouchableOpacity>
-              )}
-            </View>
-            {props.renderTrailing && props.renderTrailing()}
-          </View>
-        )
-      }
-    }
-  }, [preferenceTheme])
-
   useEffect(() => {
     if (!loaded) return 
     const networkItem = SUPPORTED_CHAINS.find((i) => i.chainID === chainId)
@@ -406,7 +252,7 @@ function App(): JSX.Element {
             </NavigationContainer>
           </QueryClientProvider>
         </MenuProvider>
-        <Toast config={toastConfig} />
+        <ToastComponent />
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
