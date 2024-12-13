42673852207b832ef676c038d017e05e1a91cd14
phongnhat19 <phongnhat19@gmail.com>
2024-12-13 17:59:50 +0700

    hot fix

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,7 +3,18 @@ import {typography} from './typography'
 const theme = {
   color,
   accentColor,
-  typography
+  typography,
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.53,
+    shadowRadius: 13.97,
+
+    elevation: 21,
+  }
 }
 
 export default theme
